745302d7eb9376c9ea41002563c8508cc1086452
sservicetk20 <sservicetk@gmail.com>
2016-09-10 22:14:10 +0000

    10/09/2016 implementacion del elemento body-parser para recibir parametros

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 ```
 
-###aprendiendo de nodejs
+
+
+### aprendiendo de nodejs
 1.interacion de node con las vistas(html,css,java etc) creando un servidor = commit mesage
 2.NPM = es el manejador de paquetes node(manejador de paquetes que uitlizamos en la aplicacion)
 3.express = libreria web 
+4.JADE se usara para sentizar el html
+5.Middleware (validador de peticiones al servidor )
+6.montar el middleware pasar el middleware como paremetro al metodo app
+7.body-parser 
+
+
+
+
+
+
+
+
 
 ```
--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var http = require("http"),
-    fs = require("fs");
-    
-var html = fs.readFileSync("./index.html");
-    
-http.createServer(function(req,res){
-    res.write(html);
-    res.end();
-}).listen(8080);
+var express = require("express");
+var bodyParser = require("body-parser");//para peticiones al servidor
 
+var app = express();
+
+// montar el middleware pasar el middleware como paremetro al metodo app
+// podemos observar todo lo que este dentro de la carpeta public
+app.use(express.static("public")); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extends: true} ));
+
+app.set("view engine", "jade");
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/users",function(req,res){
+    console.log("contraseña:"+ req.body.password);
+    console.log("Email:"+ req.body.email);
+    res.send("recibimos tus datos");
+});
+
+
+app.listen(8080);
